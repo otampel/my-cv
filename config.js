93fc2f65cd163f54d9b60f4e85e5d26d56d3a6e8
Alexander Zhukov <al.zhukoff@gmail.com>
2018-07-31 20:29:58 +0200

    add config

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "body_class": "markdown-body",
+  "marked_options": {
+    "headerIds": false,
+    "smartypants": true,
+  },
+  "pdf_options": {
+    "format": "A4",
+    "margin": "15mm 20mm"
+  },
+  "stylesheet_encoding": "utf-8"
+}
